194e234f6e69eb7d62b8208c37e5eb13b50e1c46
poster-keisuke <ta.keisuke4@gmail.com>
2021-07-28 21:15:45 +0900

    add palindrome number python

---
+++ LeetCode/9_palindrome_number.py
@@ -0,0 +1,19 @@
+#  Palindrome Number
+#  https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        if 0 < x <= 9: return True
+
+        original = x
+        num = x
+        reverse = 0
+
+        for i in str(x):
+            tmp = num % 10
+            num = num // 10
+
+            reverse = reverse * 10 + tmp
+
+        return reverse == original
