675b45414c121bf40eb0054324ab8998d86e73e7
poster-keisuke <ta.keisuke4@gmail.com>
2021-07-30 22:27:22 +0900

    add: comment

--- LeetCode/175_combine_two_tables.sql
+++ LeetCode/175_combine_two_tables.sql
@@ -1,4 +1,4 @@
--- Palindrome Number
+-- 175. Palindrome Number
 -- https://leetcode.com/problems/combine-two-tables/
 
 select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId
