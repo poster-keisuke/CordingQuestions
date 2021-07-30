452e25e557712fb19116524ad0453bd1c0d28879
poster-keisuke <ta.keisuke4@gmail.com>
2021-07-30 22:27:18 +0900

    add: solve longest common prefix

---
+++ LeetCode/14_longest_common_prefix.ts
@@ -0,0 +1,17 @@
+// 14. Longest Common Prefix
+// https://leetcode.com/problems/palindrome-number/
+
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0 || strs[0] === '') return '';
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+
+      if (prefix === '') return '';
+    }
+  }
+  return prefix;
+}
