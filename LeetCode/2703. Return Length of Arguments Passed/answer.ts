6d0b7d57d45b5ec77c834a08f6a37fc4347690cf
poster-keisuke <ta.keisuke4@gmail.com>
2023-05-29 10:07:49 +0900

    add: 2703. Return Length of Arguments Passed answer

---
+++ LeetCode/2703. Return Length of Arguments Passed/answer.ts
@@ -0,0 +1,10 @@
+// 2703. Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+
+function argumentsLength(...args: any[]): number {
+  return args.length;
+}
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
