700cfe2a044bd238841200712f17d4c65e4de854
poster-keisuke <ta.keisuke4@gmail.com>
2023-07-31 09:45:25 +0900

    add: 2798. Number of Employees Who Met the Target answer

---
+++ LeetCode/2798. Number of Employees Who Met the Target/answer.py
@@ -0,0 +1,11 @@
+# 2798. Number of Employees Who Met the Target
+# https://leetcode.com/problems/number-of-employees-who-met-the-target/
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        count = 0
+        for i in range(len(hours)):
+            if hours[i] >= target:
+                count += 1
+
+        return count
